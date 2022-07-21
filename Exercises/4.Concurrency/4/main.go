f2151311ee56c90cf18dc85321f56aad360a36a1
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-07-21 18:32:35 +0700

    Задание 4.4

--- Exercises/4.Concurrency/4/main.go
+++ Exercises/4.Concurrency/4/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 var (
 	a = []int{1, 3, 5, 7, 9}
@@ -14,5 +17,30 @@ func main() {
 }
 
 func realMain(a, b []int, c *[]int) {
-	// TODO: код писать здесь
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	chA := make(chan int, 1)
+	chB := make(chan int, 1)
+	chB <- 0
+	go oddNumbers(a, c, chA, chB, wg)
+	go numbers(b, c, chA, chB, wg)
+	wg.Wait()
+}
+
+func oddNumbers(a []int, c *[]int, chA chan int, chB chan int, wg *sync.WaitGroup) {
+	for _, value := range a {
+		<-chB
+		*c = append(*c, value)
+		chA <- value
+	}
+	wg.Done()
+}
+
+func numbers(b []int, c *[]int, chA chan int, chB chan int, wg *sync.WaitGroup) {
+	for _, value := range b {
+		<-chA
+		*c = append(*c, value)
+		chB <- value
+	}
+	wg.Done()
 }
