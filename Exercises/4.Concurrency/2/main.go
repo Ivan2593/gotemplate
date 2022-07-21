75d37c1aa65aa4386a7c36012572fe14bea9436f
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-07-21 17:51:58 +0700

    Задание 4.2

--- Exercises/4.Concurrency/2/main.go
+++ Exercises/4.Concurrency/2/main.go
@@ -1,18 +1,45 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
 const timeout = time.Second * 2
 
 func main() {
-	// TODO: код писать здесь
+	fmt.Println(realMain(1))
 }
 
 func realMain(goroutineDuration time.Duration) error {
-	// TODO: код писать здесь
+	ch := make(chan int, 2)
+	go g(ch, goroutineDuration)
+	go g(ch, goroutineDuration)
+	timer := time.NewTimer(timeout)
+	for i := 0; i < 2; i++ {
+		select {
+		case <-ch:
+			{
+			}
+		case <-timer.C:
+			{
+				return errors.New("время вышло")
+			}
+		}
+	}
+	close(ch)
 	return nil
 }
 
-// TODO: код писать здесь
+func g(ch chan int, t time.Duration) {
+	timer := time.NewTimer(timeout)
+	time.Sleep(t)
+	select {
+	case <-timer.C:
+		fmt.Println("Время вышло")
+		return
+	default:
+		ch <- 1
+	}
+}
