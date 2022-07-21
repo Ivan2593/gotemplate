2dd2f28b11db798bd0c11d3bfd70da6e4367ce22
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-07-21 16:21:07 +0700

    Задание 2.3

--- Exercises/2.Basics/3/main.go
+++ Exercises/2.Basics/3/main.go
@@ -1,9 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func main() {
+	fmt.Println(stringStat("съешь ещё этих мягких французских булок, да выпей чаю"))
 }
 
 func stringStat(word string) string {
-	// TODO: код писать здесь
-	return ""
+	symbolsMap := make(map[rune]int, 0)
+	var keys []rune
+	word = strings.ToLower(word)
+	str := ""
+	for _, value := range word {
+		if string(value) != " " {
+			symbolsMap[value]++
+			if symbolsMap[value] == 1 {
+				keys = append(keys, value)
+			}
+		}
+	}
+	for _, value := range keys {
+		str = str + string(value) + " - " + strconv.Itoa(symbolsMap[value]) + "\n"
+	}
+	str = str[0 : len(str)-1]
+	return str
 }
